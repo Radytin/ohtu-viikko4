1cf5d51b35b88e3296e494b75ad43ea13844bd3a
Radytin <Kreetta.hamalainen@helsinki.fi>
2017-04-01 13:19:04 +0300

    tehtävien aloittelua

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,32 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // vaihda oma opiskelijanumerosi seuraavaan
+        String studentNr = "014727065";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        
+        System.out.println("Oliot:");
+        for (Submission submission : subs) {
+            System.out.println(submission);
+        }
+
     }
 }
--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,8 +1,18 @@
 package ohtu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Submission {
     private String student_number;
-
+    private int hours;
+    private int week;
+    private boolean a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11;
+    
+    private List<Integer> list=new ArrayList<Integer>();
+    private int maara;
+  
+    
     public String getStudent_number() {
         return student_number;
     }
@@ -10,10 +20,45 @@ public class Submission {
     public void setStudent_number(String student_number) {
         this.student_number = student_number;
     }
-
-    @Override
-    public String toString() {
-        return student_number;
+    
+    
+    public int getHours() {
+        return hours;
     }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+     public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+     public void countDone(){
+        int maara=0;
+        if(a1){maara++;list.add(1);} if(a2){maara++;list.add(2);} if(a3){maara++;list.add(3);} if(a4){maara++;list.add(4);} if(a5){maara++;list.add(5);}
+        if(a6){maara++;list.add(6);} if(a7){maara++;list.add(7);} if(a8){maara++;list.add(8);} if(a9){maara++;list.add(9);} if(a10){maara++;list.add(10);}
+        if(a11){maara++;list.add(11);}
+        
+        this.maara=maara;
+}
+ 
+
+    
+
+    
+    
+ @Override
+    public String toString() {
+        
+        String doneS="";
+        for(Integer i:list){
+            doneS+=i+" ";
+        }
+        return "viikko "+week+": "+"tehtyjä tehtäviä yhteensä: "+maara+", aikaa kului "+hours+" tuntia, tehdyt tehtävät: "+doneS;
+}
+
     
 }
